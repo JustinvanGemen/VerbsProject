874810adf0027e4d86f241a650c1a84dbb263c05
Rico Evers <ricoevers@live.nl>
2016-10-27 10:51:41 +0200

    player input controller and player movement script

---
+++ Assets/Scripts/Player/PlayerInputController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputController : MonoBehaviour {
+
+	private Vector3 movement;
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		movement = new Vector3 (moveHorizontal, 0, moveVertical);
+	}
+
+	public Vector3 GetInput
+	{
+		get{return movement;}
+	}
+}
