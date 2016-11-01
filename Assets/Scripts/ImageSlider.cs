6096ab138737a34ffdb414a953218b6c14362121
Rico Evers <ricoevers@live.nl>
2016-11-01 09:00:46 +0100

    Game over, enemy movement, image slider and player models

---
+++ Assets/Scripts/ImageSlider.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ImageSlider : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] images;
+	private int counter = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Backwards()
+	{
+		counter--;
+
+		if(counter < 0)
+		{
+			counter = images.Length - 1;
+		}
+		DisableImages ();
+	}
+
+	public void Forwards()
+	{
+		counter++;
+		if(counter > images.Length -1)
+		{
+			counter = 0;
+		}
+		DisableImages ();
+	}
+
+	private void DisableImages()
+	{
+		for (int i = 0; i < images.Length; i++)
+		{
+			images [i].SetActive(false);
+
+			if(i == counter)
+			{
+				images [i].SetActive(true);
+			}
+		}
+	}
+}
