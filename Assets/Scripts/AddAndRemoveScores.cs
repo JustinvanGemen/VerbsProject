e94758c483818a79b581b8310bd5f2643567f7d0
jvdwijk <joeyvdwijk@gmail.com>
2016-11-06 17:47:48 +0100

    score scripts
    added SendScore to send scores to the database
    added RetrieveScores to get back highscores from the database
    added AddAndRemove to update the score of the player

---
+++ Assets/Scripts/AddAndRemoveScores.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddAndRemoveScores : MonoBehaviour {
+    private int score;
+    public int Score { get { return score; } set { score = value; } }
+	
+	void Start () {
+        score = 0;
+	}
+
+    public void ChangeScore(int pointsAmount)
+    {
+        score = score + pointsAmount;
+    }	
+}
---
+++ Assets/Scripts/RetrieveScores.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RetrieveScores : MonoBehaviour {
+    private string[] highScores;
+
+    
+    IEnumerator Start () {
+        WWW dataInformation = new WWW("link");
+        yield return dataInformation;
+        string textData = dataInformation.text;
+        highScores = textData.Split(";"[0]);
+    }
+}
