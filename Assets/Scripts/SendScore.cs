62a735cd8ce504f5892ff2745545835f559e6337
jvdwijk <joeyvdwijk@gmail.com>
2016-11-08 10:22:29 +0100

    sendscore link update

--- Assets/Scripts/SendScore.cs
+++ Assets/Scripts/SendScore.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class SendScore : MonoBehaviour {
     	
 	public IEnumerator EnterEndScore (int score, string playerID) {
-        string score_url = "link" + "?score=" + score;
+		string score_url = "http://jvdwijk.com/Games/Verbs/database.php" + "?id=" + playerID + "&score=" + score;
         WWW webRequest = new WWW(score_url);
         yield return webRequest;
     }
