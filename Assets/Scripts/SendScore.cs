5c5956e6191b79702777d0e4c30926e0b73b835d
jvdwijk <joeyvdwijk@gmail.com>
2016-11-08 11:23:18 +0100

    sendscore update and credits start

--- Assets/Scripts/SendScore.cs
+++ Assets/Scripts/SendScore.cs
@@ -1,8 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SendScore : MonoBehaviour {
     	
+	private AddAndRemoveScores addAndRemoveScores;
+	private int score;
+
+	public void EnterScore(InputField winnerName)
+	{
+		Debug.Log(winnerName.text);
+		if(gameObject != null)
+		{
+			addAndRemoveScores = GetComponent<AddAndRemoveScores>();
+			score = addAndRemoveScores.Score;
+			score = Mathf.Round(score);
+			StartCoroutine(EnterEndScore(score, winnerName.text));
+		}
+
+	}
+
 	public IEnumerator EnterEndScore (int score, string playerID) {
 		string score_url = "http://jvdwijk.com/Games/Verbs/database.php" + "?id=" + playerID + "&score=" + score;
         WWW webRequest = new WWW(score_url);
